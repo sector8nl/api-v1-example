d3d02f512f1c75058114cbc55f0c7e1e4230b999
marcel <marcel@grofas.com>
2015-11-02 15:17:31 +0100

    -

--- config.php
+++ config.php
@@ -5,5 +5,5 @@ ini_set('error_reporting', E_ALL);
 
 $config = [
     'endpoint' => 'https://app.trengo.eu/api/v1/',
-    'token' => 'HnLKMFbBBtJ5xpWd3fOHYIAcFrccnkn3YQyFIJbRKCI8bCsdvqVhfOQXHlZHkjrx13ukUVPrBpC'
+    'token' => '*secret*'
 ];
